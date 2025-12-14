123e7def2fc445aec6e7d87394bb423087e015e6
artemsugak37 <m.sugak37@gmail.com>
2025-12-14 12:06:55 +0300

    [f

--- VibeFinance/app.py
+++ VibeFinance/app.py
@@ -152,7 +152,7 @@ def init_db():
         print("База данных users.db уже существует. Проверены все таблицы.")
 @app.route('/')
 def index():
-    with open('D:\артём\clown project\VibeFinance\main.html', 'r', encoding='utf-8') as f:
+    with open('D:/артём/clown project/VibeFinance/main.html', 'r', encoding='utf-8') as f:
         return f.read()
 
 @app.route('/register', methods=['POST'])
